3ef2108f025913835fba0f738c342a9355c53363
Zeppelin-V <zvanbarr@ucsd.edu>
2019-06-10 23:55:33 -0700

    Added some code for test_model function for running model through testing data. TODO: Need to implement accuracy/loss metrics.

--- train.py
+++ train.py
@@ -118,6 +118,7 @@ def train_model(model_name, computing_device, val_indices, epochs, k, learning_r
                     avg_acc = torch.mea((train_acc.to(dtype=torch.float) / (len(output))).float())
                     train_accuracy.append(avg_acc)
 
+
             #Add this to the list of average training minibatch loss to list for all val/training splits
             train_epoch_loss.append(train_total_loss)
             avg_train_mb_epoch_loss.append(avg_train_minibatch_loss)
@@ -168,6 +169,7 @@ def train_model(model_name, computing_device, val_indices, epochs, k, learning_r
             val_epoch_acc.append(val_accuracy)
 
 
+        #Record this train/validation splits loss/accuracy metrics
         all_val_total_loss.append(val_epoch_loss)
         all_train_total_loss.append(train_epoch_loss)
         all_val_avg_minibatch_loss.append(avg_val_mb_epoch_loss)
@@ -180,11 +182,105 @@ def train_model(model_name, computing_device, val_indices, epochs, k, learning_r
             all_val_acc, all_train_total_loss, all_train_avg_minibatch_loss, all_train_acc)
 
 
-    
 
 
+def test_model(model_name, computing_device, test_loader, train_inidices, epochs, learning_rate, batch_size,
+               num_minibatches, wvlt_transform, transform, extras):
+
+    #Define the loss criterion and instantiate the gradient descent optimizer
+    criterion = nn.CrossEntropyLoss()
+
+    #Get a new model for this training split
+    model = get_model(model_name, computing_device, wvlt_transform)
+
+    #Instantiate the gradient descent optimizer - use Adam optimizer
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    #Get training dataloader
+    train_loader = img_dataloader.create_train_dataloader(train_inidices, batch_size, transform,
+                                                          show_sample=False, extras=extras)
+
+    #Record train_loss and avg_train_mb loss
+    train_total_loss = []
+    avg_train_minibatch_loss = []
+
+    #Train for x epochs
+    for epoch in range(0, epochs):
+
+        N = num_minibatches
+        N_minibatch_loss = 0.0
+
+        print("Training model on training set for epoch num: ", epoch)
+
+        #Run train data through model
+        for minibatch_count, (images, labels) in enumerate(train_loader):
+
+            #Put the minibatch data in CUDA Tensors and run on the GPU if possible
+            images, labels = images.to(computing_device), labels.to(computing_device)
+
+            #Zero out the stored gradient (buffer) from the previous iteration
+            optimizer.zero_grad()
+
+            #Perform the forward pass through the network and compute the loss
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
+            #Backprop XD
+            loss.backward()
+
+            #Update the weights
+            optimizer.step()
+
+            #Add this iteration's loss to the total_loss
+            train_total_loss.append(loss.item())
+            N_minibatch_loss += loss
+
+            if minibatch_count % N == 0:
+
+                #Print the loss averaged over the last N mini-batches
+                N_minibatch_loss /= N
+                print("Epoch ", str(epoch+1), " avg minibatch # ", minibatch_count, " loss: ", N_minibatch_loss)
 
 
+                #Add the averaged loss over N_minibatches and reset the counter
+                avg_train_minibatch_loss.append(N_minibatch_loss)
+
+                N_minibatch_loss = 0.0
+
+
+        N_minibatch_loss = 0.0
+
+        test_total_loss = []
+        test_avg_minibatch_loss = []
+
+        #Iterate through testing minibatches
+        for minibatch_count, (images,labels) in enumerate(test_loader, 0):
+
+            #Get labels and images on CUDA
+            images, labels = images.to(computing_device), labels.to(computing_device)
+
+            #Forward pass with no gradients to save memory
+            with torch.no_grad():
+                output = model(images)
+            loss = criterion(output, labels)
+
+            #Add this iteration's loss to the total_loss
+            test_total_loss.append(loss.item())
+            N_minibatch_loss += loss
+
+            if minibatch_count % N == 0:
+                #Print the loss averaged over the last N minibatches
+                print("Epoch ", str(epoch + 1), " average minibatch # ", minibatch_count, " loss: ", N_minibatch_loss)
+
+                #Add the averaged loss over N minibatches and reset the counter
+                test_avg_minibatch_loss.append(N_minibatch_loss)
+
+                N_minibatch_loss = 0.0
+
+
+            #TODO: Implement the accuracy/loss metrics for testing the model
+
+            return 0
 
 
 
