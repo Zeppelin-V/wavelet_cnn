1b56d2fb993ad177df10c1b4d5467abe129f0947
Zeppelin-V <zvanbarr@ucsd.edu>
2019-06-11 17:03:00 -0700

    Fixing tensor operations in wavelet transform

--- wavelet.py
+++ wavelet.py
@@ -21,10 +21,10 @@ class wavelet_transform():
 
         #Create a 3 dimensional tensor, where each output coefficients array is stacked
         #Ordering is LL, LH, HL HH
-        return torch.stack((torch.unsqueeze(torch.Tensor(coeffs[0]).to(self.computing_device), 1),
-                            torch.unsqueeze(torch.Tensor(coeffs[1]).to(self.computing_device), 1),
-                            torch.unsqueeze(torch.Tensor(coeffs[2]).to(self.computing_device), 1),
-                            torch.unsqueeze(torch.Tensor(coeffs[3]).to(self.computing_device), 1)), dim=2)
+        return torch.stack((torch.unsqueeze(torch.Tensor(coeffs[0]).to(self.computing_device), 2),
+                            torch.unsqueeze(torch.Tensor(coeffs[1][0]).to(self.computing_device), 2),
+                            torch.unsqueeze(torch.Tensor(coeffs[1][1]).to(self.computing_device), 2),
+                            torch.unsqueeze(torch.Tensor(coeffs[1][2]).to(self.computing_device), 2)), dim=2)
 
 
     #Apply the wavelet transform over an image batch
