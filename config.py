831e565e912861d824dd26f7c3c9d4d8f1a4e250
Zeppelin-V <zvanbarr@ucsd.edu>
2019-06-14 13:40:21 -0700

    Testing output metrics for test

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 config = {
     'model_name': "WaveletCNN",
     'wvlt_transform': 'db1',
-    'num_epochs':  1,
+    'num_epochs':  20,
     'batch_size': 8,
     'learning_rate': 0.0001,
     'num_mb': 50,
