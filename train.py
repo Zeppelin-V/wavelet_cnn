a867e15187c7dec0f2cec0e98bd27b4c6d054eaf
Zeppelin-V <zvanbarr@ucsd.edu>
2019-06-14 13:10:56 -0700

    Testing output metrics for test

--- train.py
+++ train.py
@@ -425,6 +425,9 @@ def output_test_metrics(accuracy_metrics, outname):
 
             for val in range(len(accuracy_metrics[i])):
 
+                print(accuracy_metrics[i])
+
+
                 file.write(str(accuracy_metrics[i][val].item))
 
                 if val != (len(accuracy_metrics[i]) - 1):
