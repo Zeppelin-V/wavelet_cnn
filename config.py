38da6ac4841a95e6c1a8abc90249167cf769d83d
Zeppelin-V <zvanbarr@ucsd.edu>
2019-06-13 22:35:14 -0700

    Testing db1 WaveletCNN model

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 config = {
-    'model_name': "BaselineCNN",
-    'wvlt_transform': 'coif1',
+    'model_name': "WaveletCNN",
+    'wvlt_transform': 'db1',
     'num_epochs': 20,
     'batch_size': 8,
     'learning_rate': 0.0001,
@@ -8,6 +8,6 @@ config = {
     'seed': 1,
     'k': 2,
     'p_test': 0.1,
-    'test': False,
-    'outname': 'baseline_train'
+    'test': True,
+    'outname': 'test'
 }
